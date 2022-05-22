1ad7f406b00db567745048d3ecf347eba41f6b86
Andrew <andrew.am2017@gmail.com>
2022-05-23 00:14:47 +0300

    переобределил метод __str__ для моделей и создал пробную статью

--- myfirst/myfirst/apps/articles/models.py
+++ myfirst/myfirst/apps/articles/models.py
@@ -8,7 +8,13 @@ class Article(models.Model):
     article_text = models.TextField('текст статьи')
     pub_date = models.DateTimeField('дата публикации')
 
+    def __str__(self):
+        return self.article_titles
+
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     autor_name = models.CharField('имя автора', max_length=50)
     comment_text = models.CharField('текст комментария', max_length=200)
+
+    def __str__(self):
+        return self.autor_name
