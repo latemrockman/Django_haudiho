d71998ebc48bacb1bd9b8356dd786c991eaf992e
Andrew <andrew.am2017@gmail.com>
2022-05-22 23:21:29 +0300

    подключил urls

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Hello Andrew')
+
+
+def test(request):
+    return HttpResponse('Test')
+
+
+
