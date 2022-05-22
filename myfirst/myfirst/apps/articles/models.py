17863068a10e19bc40ec38605cd7761decf7f7bf
Andrew <andrew.am2017@gmail.com>
2022-05-23 00:20:23 +0300

    добавил метод was_published_recently в Aticle

--- myfirst/myfirst/apps/articles/models.py
+++ myfirst/myfirst/apps/articles/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -10,6 +12,10 @@ class Article(models.Model):
 
     def __str__(self):
         return self.article_titles
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days = 7))
+
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
