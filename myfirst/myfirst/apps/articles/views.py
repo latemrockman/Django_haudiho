7a2c424256eb7947d4a265fbbbf51cf435b6044d
Andrew <andrew.am2017@gmail.com>
2022-05-24 13:01:18 +0300

    подключил шаблон главной страницы

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello Andrew')
+    return render(request, 'articles/list.html', {})
 
 
 def test(request):
