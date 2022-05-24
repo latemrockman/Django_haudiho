67d5a5588283f646ebf79135789cfffbe098b082
Andrew <andrew.am2017@gmail.com>
2022-05-24 13:59:53 +0300

    вывел список статей на главную и подключил страницу статей

--- myfirst/myfirst/apps/articles/urls.py
+++ myfirst/myfirst/apps/articles/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:article_id>/', views.article_page, name='article-page'),
     path('test/', views.test, name='test')
 ]
--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article, Comment
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'articles/list.html', {})
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'articles/list.html', {'all_articles': latest_articles_list})
+
+
+def article_page(request, article_id):
+    #return HttpResponse(f'Test - {article_id}')
+
+    return render(request, 'articles/article_page.html', {})
 
 
 def test(request):
